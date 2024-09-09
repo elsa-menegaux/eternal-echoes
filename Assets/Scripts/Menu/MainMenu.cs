e9131a9cdd0512752d8a6e6d8f5d99769b5de15f
IDuzTheGamez <jeffery.tolmie@gmail.com>
2024-09-09 13:45:32 +1200

    Hide Continue Button, Add scene changing for new game

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,9 +1,18 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void quitGame()
+    
+
+    public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
