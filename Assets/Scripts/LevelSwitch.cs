4c9cd1c9e28dbe09be9b7d7d9978676b05a09182
TheGuyNamedJosh <theguynamedjosh1@gmail.com>
2024-09-20 20:43:30 +1200

    Add 'random' level generation

--- Assets/Scripts/LevelSwitch.cs
+++ Assets/Scripts/LevelSwitch.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSwitch : MonoBehaviour
 {
 
     public int sceneBuildIndex;
+
+    public bool randomIndex = true;
+    public bool banSameRoom = true;
+    public List<string> roomScenes = new List<string>{"Room 1","Room 2","Room 3","Room 4","Room 5"};
     
     // If Exit zone is entered and collider is a player
     // Scene changes to another room without needing input
@@ -19,7 +25,26 @@ public class LevelSwitch : MonoBehaviour
         if (entity.CompareTag("Player"))
         {
             Debug.Log("Player detected");
-            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneBuildIndex);
+            if (randomIndex)
+            {
+                string scene = "";
+                int roomNum = UnityEngine.Random.Range(0,roomScenes.Count-1);
+                scene = roomScenes.ToArray()[roomNum];
+                if (banSameRoom && scene == SceneManager.GetActiveScene().name){
+                    while(scene == SceneManager.GetActiveScene().name)
+                    {
+                        roomNum = UnityEngine.Random.Range(0,roomScenes.Count-1);
+                        scene = roomScenes.ToArray()[roomNum];
+                    }
+                }
+                
+
+                UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+
+            } else{
+                UnityEngine.SceneManagement.SceneManager.LoadScene(sceneBuildIndex);
+            }
+            
         }
     }
 }
