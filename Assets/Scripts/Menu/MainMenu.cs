826234c6427fd16ec9f2023ff5ab9791c2e1a750
Jeffery Tolmie <jeffery.tolmie@gmail.com>
2024-10-18 16:57:13 +1300

    enable continue button

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,9 +1,14 @@
 using System.Collections;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public string saveFile = "save.json";
+    public Button continueButton;
+
     public void QuitGame()
     {
         Application.Quit();
@@ -21,5 +26,21 @@ public class MainMenu : MonoBehaviour
         yield return new WaitForSeconds(1);
         DontDestroyOnLoadDestroyer.killAllObjects = false;
         SceneManager.LoadScene("Lobby");
+    }
+
+    private void Start()
+    {
+        if (continueButton == null) 
+        {
+            Debug.LogWarning("MainMenu: ContinueButton Not Assigned\nCannot show/hide based on existence of save file.");
+            return;
+        }
+        string fullPath = Path.Join(Application.persistentDataPath, saveFile);
+        if(File.Exists(fullPath))
+        {
+            continueButton.gameObject.SetActive(true);
+        } else {
+            continueButton.gameObject.SetActive(false);
+        }
     }
 }
