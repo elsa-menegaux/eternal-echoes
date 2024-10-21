cdd54e37d995c9aca83596737c539cedd40238a8
eff3ry <jeffery.tolmie@gmail.com>
2024-10-21 23:45:42 +1300

    Basic Colour Setting Class & Colour Data Class

---
+++ Assets/Scripts/DataPersistence/Data/PlayerColourData.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class PlayerColourData
+{
+    // Private fields for encapsulation
+    private Color baseColour;
+    private Color hairColour;
+    private Color bootsColour;
+    private Color pantsColour;
+    private Color jacketColour;
+
+    // Static field holding default colors
+    public static readonly PlayerColourData DefaultColours = new PlayerColourData(
+        new Color32(255,224,201,255),   // Default base color
+        new Color32(123, 80, 0 ,255),   // Default hair color
+        Color.gray,    // Default boots color
+        Color.blue,    // Default pants color
+        Color.red      // Default jacket color
+    );
+
+    // Parameterless constructor (uses default colours)
+    public PlayerColourData() : this(DefaultColours.baseColour, DefaultColours.hairColour, DefaultColours.bootsColour, DefaultColours.pantsColour, DefaultColours.jacketColour)
+    {
+    }
+
+    // Constructor with parameters
+    public PlayerColourData(Color baseColour, Color hairColour, Color bootsColour, Color pantsColour, Color jacketColour)
+    {
+        this.baseColour = baseColour;
+        this.hairColour = hairColour;
+        this.bootsColour = bootsColour;
+        this.pantsColour = pantsColour;
+        this.jacketColour = jacketColour;
+    }
+
+    // Encapsulated properties
+    public Color BaseColour
+    {
+        get { return baseColour; }
+        set { baseColour = value; }
+    }
+
+    public Color HairColour
+    {
+        get { return hairColour; }
+        set { hairColour = value; }
+    }
+
+    public Color BootsColour
+    {
+        get { return bootsColour; }
+        set { bootsColour = value; }
+    }
+
+    public Color PantsColour
+    {
+        get { return pantsColour; }
+        set { pantsColour = value; }
+    }
+
+    public Color JacketColour
+    {
+        get { return jacketColour; }
+        set { jacketColour = value; }
+    }
+}
