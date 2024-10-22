7f3ee552f494ff6c5240f38f512585721ec023f0
Elsa Menegaux <elsa.menegaux@free.fr>
2024-10-22 18:23:12 +1300

    set gameWon to false when loading new save

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -17,7 +17,7 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         DontDestroyOnLoadDestroyer.killAllObjects = true;
-        
+        GameManager.instance.gameWon = false;
         StartCoroutine("LoadLobby");
     }
 
