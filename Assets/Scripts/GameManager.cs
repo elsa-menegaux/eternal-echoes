b2bdb7309b86826886fe63e08da260fde6b70a18
eff3ry <jeffery.tolmie@gmail.com>
2024-10-22 23:15:41 +1300

    Fix Continue button not showing when valid save exists

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour, IDataPersistence
 {
     public static GameManager instance;
     public int roomsVisited = 0;
-    public bool gameWon = true;
+    public bool gameWon = false;
     
     public Dictionary<string, bool> enemyStatus = new Dictionary<string, bool>();
 
