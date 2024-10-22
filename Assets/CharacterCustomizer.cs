f7a3c56fb211af36c61b758fdd9dabc9588318db
eff3ry <jeffery.tolmie@gmail.com>
2024-10-22 16:12:45 +1300

    Working Character Customize Scene

---
+++ Assets/CharacterCustomizer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCustomizer : MonoBehaviour
+{
+    [Header("Player Sprite Controller")]
+    public PlayerColourController playerColourController;
+    [Header("UI Controllers")]
+    public RGBControler hairController;
+    public RGBControler baseController;
+    public RGBControler jacketController;
+    public RGBControler pantsController;
+    public RGBControler bootsController;
+
+    private PlayerColourData playerColourData;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    private void Start()
+    {
+        playerColourData = PlayerColourData.DefaultColours;
+        hairController.Colour = playerColourData.HairColour;
+        baseController.Colour = playerColourData.BaseColour;
+        jacketController.Colour = playerColourData.JacketColour;
+        pantsController.Colour = playerColourData.PantsColour;
+        bootsController.Colour = playerColourData.BootsColour;
+    }
+
+    private void Update()
+    {
+        playerColourData = new PlayerColourData(baseController.Colour, 
+            hairController.Colour, 
+            bootsController.Colour, 
+            pantsController.Colour, 
+            jacketController.Colour);
+
+        playerColourController.SetColours(playerColourData);
+    }
+}
